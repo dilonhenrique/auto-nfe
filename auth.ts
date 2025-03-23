4a24fcac85afbd5e4338ddd5b0a9b9a35faf9f02
Dilon Henrique <dilonhenrique@gmail.com>
2025-03-23 01:21:26 -0300

    type for session without null

--- auth.ts
+++ auth.ts
@@ -2,6 +2,20 @@ import { AuthOptions } from "next-auth";
 import AzureADProvider from "next-auth/providers/azure-ad";
 
 export const authOptions: AuthOptions = {
+  callbacks: {
+    session({ session }) {
+      return {
+        ...session,
+        user: session.user
+          ? {
+              name: session.user?.name ?? undefined,
+              email: session.user?.email ?? undefined,
+              image: session.user?.image ?? undefined,
+            }
+          : undefined,
+      };
+    },
+  },
   providers: [
     AzureADProvider({
       clientId: process.env.AZURE_AD_CLIENT_ID!,
